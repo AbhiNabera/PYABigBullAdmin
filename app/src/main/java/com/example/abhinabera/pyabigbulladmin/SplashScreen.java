b071692a265994012b2153b9d849e27a2b0847d9
AbhiNabera <abhinandannabera@yahoo.in>
2018-12-31 15:48:14 +0530

    Splash Screen Activity and Permissions

--- app/src/main/java/com/example/abhinabera/pyabigbulladmin/SplashScreen.java
+++ app/src/main/java/com/example/abhinabera/pyabigbulladmin/SplashScreen.java
@@ -1,13 +1,105 @@
 package com.example.abhinabera.pyabigbulladmin;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView splashImage;
+
+    ProgressBar progressBar;
+
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
+    int PERMISSION_ALL = 1;
+
+    String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_SMS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        splashImage = (ImageView) findViewById(R.id.splashImage);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+
+        else{
+            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(i);
+            overridePendingTransition(R.anim.enter, R.anim.exit);
+        }
+
     }
+
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSION_ALL) {
+            if (grantResults.length > 0) {
+
+                boolean network = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean wifi = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                boolean internet = grantResults[2] == PackageManager.PERMISSION_GRANTED;
+                boolean sms = grantResults[3] == PackageManager.PERMISSION_GRANTED;
+
+                if(!network || !wifi || !internet || !sms) {
+
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+
+                }
+                else {
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.enter, R.anim.exit);
+                }
+
+            }
+            else {
+
+                //ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+
+            }
+        }
+    }
+
+
+
+
 }
